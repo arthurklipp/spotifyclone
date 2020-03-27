37d754c04816c6268e7a49d9a9e0168c28dddefe
SrChokito <arthurklipp@gmail.com>
2020-03-27 20:03:31 -0300

    criação da pagina home e capa de album

---
+++ src/Pages/Home/index.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import './home.css';
+
+
+function head(){
+  
+    return (<div className='head'>
+              <img className='logo' src='Spotify_Logo_RGB_Black.png'/>
+            </div>
+    );
+}
+
+export class Home extends React.Component{
+  constructor(props){
+    super(props);
+    this.state={
+      capa:['albums/albumArt.png','albums/albumArt2.png','albums/albumArt3.png','albums/albumArt4.png','albums/albumArt5.png','albums/albumArt6.png'],
+      album:['Unplugged','Stadium Arcadium', 'Use Your Ilusion II','Album 1','Album 2', 'Album 3'],
+      artista:['Alice In Chains','Red Hot Chili Peppers','Guns N'+' roses','John','Peter','Alice']
+    };
+  }
+  render(){
+    return <div>
+      <Albums capa={this.state.capa} album={this.state.album} artista={this.state.artista}/>
+    </div>
+  }
+}
+
+class Albums extends React.Component{
+  render(){
+    return <div>
+      <div className='album'>
+        <img className='albumArt'src={this.props.capa[0]}/>
+        <div className='textoAlbum'>{this.props.album[0]}</div>
+        <div className='textoArtista'>{this.props.artista[0]}</div>
+      </div>
+      <div className='album'>
+        <img className='albumArt'src={this.props.capa[1]}/>
+        <div className='textoAlbum'>{this.props.album[1]}</div>
+        <div className='textoArtista'>{this.props.artista[1]}</div>
+      </div>
+      <div className='album'>
+        <img className='albumArt'src={this.props.capa[2]}/>
+        <div className='textoAlbum'>{this.props.album[2]}</div>
+        <div className='textoArtista'>{this.props.artista[2]}</div>
+      </div>
+    </div>
+  }
+}
--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Login from './Pages/Login/index';
+import {Home} from './Pages/Home/index';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Login />
+    <Home />
   </React.StrictMode>,
   document.getElementById('root')
 );
