c5109df6f65a918f729b85e05594307abf231fa7
SrChokito <arthurklipp@gmail.com>
2020-11-19 22:09:12 -0300

    rota de troca de senha

--- server/src/controllers/authController.js
+++ server/src/controllers/authController.js
@@ -3,6 +3,8 @@ const User = require('../models/User');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const crypto = require('crypto');
+const mailer = require('../modules/mailer');
 
 const authConfig = require('../config/auth');
 
@@ -46,4 +48,71 @@ router.post('/login', async (req, res) =>{
     });
 });
 
+router.post('/forgot_password', async(req, res) => {
+    const { email } = req.body;
+    try{
+        const user = await User.findOne({ email });
+
+        if(!user)
+            return res.status(400).send({error: 'User not found'});
+        
+        const token = crypto.randomBytes(20).toString('hex');
+
+        const now = new Date();
+        now.setHours(now.getHours()+1);
+
+        await User.findByIdAndUpdate(user.id,{
+            '$set':{
+                passwordResetToken: token,
+                passwordResetExpires: now,
+            }
+        });
+
+        mailer.sendMail({
+            to: email,
+            from:'spotify@spotifyclone.com.br',
+            template:'auth/forgot_password',
+            context:{ token },
+        }, (err) => {
+            if(err)
+                return res.status(400).send({error: 'Cannot send forgot password email'});
+            
+            return res.send();
+        })
+
+    }catch(err){
+        res.status(400).send({ error: 'Error on forgot password, try again'});
+    }
+});
+
+router.post('/reset_password',async (req,res) => {
+    
+    const {email, token, password} = req.body;
+
+    try{
+
+        const user = await User.findOne({ email })
+        .select('+passwordResetToken passwordResetExpires');
+
+        if(!user)
+            return res.status(400).send({error: 'User not found'});
+
+        if(token !== user.passwordResetToken)
+            return res.status(400).send({error: 'Invalid token'});
+
+        const now = new Date();
+
+        if(now > user.passwordResetExpires)
+            return res.status(400).send({error: 'Token expired, please generate a new one'});
+        
+        user.password = password;
+
+        await user.save();
+
+        res.send();
+    }catch(err){
+        res.status(400).send({ error: 'Cannot reset password, try again'});
+    }
+
+});
 module.exports = app => app.use('/auth', router);
