a6a677c8135dd0712210120cb6aee86394470d4e
arthur klipp zenzeluk <Hellraiser>
2021-06-28 11:49:35 -0300

    atualizacao

--- server/src/controllers/projectController.js
+++ server/src/controllers/projectController.js
@@ -4,6 +4,9 @@ const multer = require('multer');
 const multerConfig = require('../config/multer');
 const router = express.Router();
 const User = require('../models/User');
+const fs = require("fs");
+const path = require("path");
+const { promisify } = require("util");
 
 router.use(authMiddleware);
 
@@ -13,12 +16,14 @@ router.get('/',(req,res) =>{
 
 router.post('/post', multer(multerConfig).single('file'), async (req,res) =>{
         const id = req.userId;
-        User.findOneAndUpdate(id, {$set:{perfil:"http://localhost:8080/uploads/"+req.file.filename}},function(err, doc){
+        const perfil = await User.findById(id);
+        promisify(fs.unlink)(
+            path.resolve(__dirname, '..', '..', 'public', 'imgs', 'perfil', perfil.perfil)
+          );
+        User.findOneAndUpdate(id, {$set:{perfil:req.file.filename}},function(err, doc){
             if(err){
                 console.log("Something wrong when updating data!");
             }
-        
-            console.log(doc);
         });
         
     res.send({ok: true, perfil: "http://localhost:8080/uploads/"+req.file.filename})
